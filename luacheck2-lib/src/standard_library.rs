b92e54d7c524575eaf9c2f35a576a8b7724c4578
Kampfkarren <boynedmaster@gmail.com>
2019-10-11 01:48:15 -0700

    Finish deseerialize implementation, lua51.toml passes!

--- luacheck2-lib/src/standard_library.rs
+++ luacheck2-lib/src/standard_library.rs
@@ -1,6 +1,9 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
 
-use serde::{de::{self, Deserializer}, Deserialize};
+use serde::{
+    de::{self, Deserializer, Visitor},
+    Deserialize,
+};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
 pub struct StandardLibrary {
@@ -9,22 +12,35 @@ pub struct StandardLibrary {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Field {
-    name: String,
-    field_type: FieldType,
+pub enum Field {
+    Function(Vec<Argument>),
+    Property,
+    Table(HashMap<String, Field>),
 }
 
 impl<'de> Deserialize<'de> for Field {
     fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let field_raw = FieldSerde::deserialize(&deserializer)?;
+        let field_raw = FieldSerde::deserialize(deserializer)?;
 
         if !field_raw.property && field_raw.args.is_none() && field_raw.children.is_empty() {
-            return Err(de::Error::custom("can't determine what kind of field this is"));
+            return Err(de::Error::custom(
+                "can't determine what kind of field this is",
+            ));
         }
 
         if field_raw.property && field_raw.args.is_some() {
             return Err(de::Error::custom("field is both a property and a function"));
         }
+
+        if field_raw.property {
+            return Ok(Field::Property);
+        }
+
+        if let Some(args) = field_raw.args {
+            return Ok(Field::Function(args));
+        }
+
+        Ok(Field::Table(field_raw.children))
     }
 }
 
@@ -33,21 +49,16 @@ struct FieldSerde {
     #[serde(default)]
     property: bool,
     #[serde(default)]
-    args: Option<HashMap<String, Argument>>,
+    args: Option<Vec<Argument>>,
     #[serde(flatten)]
     children: HashMap<String, Field>,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum FieldType {
-    Function(Vec<Argument>),
-    Property,
-    Table(Vec<Field>),
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
 pub struct Argument {
+    #[serde(default)]
     required: Required,
+    #[serde(rename = "type")]
     argument_type: ArgumentType,
 }
 
@@ -63,4 +74,49 @@ pub enum ArgumentType {
 pub enum Required {
     NotRequired,
     Required(Option<String>),
+}
+
+impl Default for Required {
+    fn default() -> Self {
+        Required::NotRequired
+    }
+}
+
+impl<'de> Deserialize<'de> for Required {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_any(RequiredVisitor)
+    }
+}
+
+struct RequiredVisitor;
+
+impl<'de> Visitor<'de> for RequiredVisitor {
+    type Value = Required;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a boolean or a string message (when required)")
+    }
+
+    fn visit_bool<E: de::Error>(self, value: bool) -> Result<Self::Value, E> {
+        if value {
+            Ok(Required::Required(None))
+        } else {
+            Ok(Required::NotRequired)
+        }
+    }
+
+    fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
+        Ok(Required::Required(Some(value.to_owned())))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_serde() {
+        toml::from_str::<StandardLibrary>(include_str!("../../luacheck2/standards/lua51.toml"))
+            .expect("lua51.toml did not deserialize as StandardLibrary");
+    }
 }
