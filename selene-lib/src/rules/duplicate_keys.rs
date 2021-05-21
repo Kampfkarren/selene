a4beff200189428e56e96fbc0c4a44b19aa42fb6
JohnnyMorganz <johnnymorganz@outlook.com>
2021-05-21 17:33:28 +0100

    Remove panic

--- selene-lib/src/rules/duplicate_keys.rs
+++ selene-lib/src/rules/duplicate_keys.rs
@@ -151,7 +151,7 @@ impl Visitor<'_> for DuplicateKeysVisitor {
                     self.check_field(&mut declared_fields, key, field_range)
                 }
 
-                other => panic!("unknown node {:?}", other),
+                other => {}
             }
         }
     }
