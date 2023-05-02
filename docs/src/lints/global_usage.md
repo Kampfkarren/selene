cbf1a40a69b7aa7740aa835249249aaf96b26cba
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-05-02 15:40:12 -0700

    Add ignore_pattern config to global_usage

--- docs/src/lints/global_usage.md
+++ docs/src/lints/global_usage.md
@@ -5,6 +5,9 @@ Prohibits use of `_G`.
 ## Why this is bad
 `_G` is global mutable state, which is heavily regarded as harmful. You should instead refactor your code to be more modular in nature.
 
+## Configuration
+`ignore_pattern` (default: `".^"`) - A [regular expression](https://en.wikipedia.org/wiki/Regular_expression) for variables that are allowed to be global variables. The default disallows all global variables regardless of their name.
+
 ## Remarks
 If you are using the Roblox standard library, use of `shared` is prohibited under this lint.
 
--- selene-lib/src/lints/global_usage.rs
+++ selene-lib/src/lints/global_usage.rs
@@ -1,23 +1,43 @@
 use super::*;
-use std::{collections::HashSet, convert::Infallible};
+use std::collections::HashSet;
 
 use full_moon::ast::Ast;
+use regex::Regex;
+use serde::Deserialize;
 
 fn is_global(name: &str, roblox: bool) -> bool {
     (roblox && name == "shared") || name == "_G"
 }
 
-pub struct GlobalLint;
+#[derive(Clone, Deserialize)]
+#[serde(default)]
+pub struct GlobalConfig {
+    ignore_pattern: String,
+}
+
+impl Default for GlobalConfig {
+    fn default() -> Self {
+        Self {
+            ignore_pattern: ".^".to_owned(),
+        }
+    }
+}
+
+pub struct GlobalLint {
+    ignore_pattern: Regex,
+}
 
 impl Lint for GlobalLint {
-    type Config = ();
-    type Error = Infallible;
+    type Config = GlobalConfig;
+    type Error = regex::Error;
 
     const SEVERITY: Severity = Severity::Warning;
     const LINT_TYPE: LintType = LintType::Complexity;
 
-    fn new(_: Self::Config) -> Result<Self, Self::Error> {
-        Ok(GlobalLint)
+    fn new(config: Self::Config) -> Result<Self, Self::Error> {
+        Ok(GlobalLint {
+            ignore_pattern: Regex::new(&config.ignore_pattern)?,
+        })
     }
 
     fn pass(&self, _: &Ast, context: &Context, ast_context: &AstContext) -> Vec<Diagnostic> {
@@ -30,7 +50,23 @@ impl Lint for GlobalLint {
             .filter(|(_, reference)| {
                 if !checked.contains(&reference.identifier) {
                     checked.insert(reference.identifier);
-                    is_global(&reference.name, context.is_roblox()) && reference.resolved.is_none()
+
+                    let matches_ignore_pattern = match reference
+                        .indexing
+                        .as_ref()
+                        .and_then(|indexing| indexing.first())
+                        .and_then(|index_entry| index_entry.static_name.as_ref())
+                    {
+                        // Trim whitespace at the end as `_G.a  = 1` yields `a  `
+                        Some(name) => self
+                            .ignore_pattern
+                            .is_match(&name.to_string().trim_end_matches(char::is_whitespace)),
+                        None => false,
+                    };
+
+                    is_global(&reference.name, context.is_roblox())
+                        && !matches_ignore_pattern
+                        && reference.resolved.is_none()
                 } else {
                     false
                 }
@@ -55,6 +91,22 @@ mod tests {
 
     #[test]
     fn test_global_usage() {
-        test_lint(GlobalLint::new(()).unwrap(), "global_usage", "global_usage");
+        test_lint(
+            GlobalLint::new(GlobalConfig::default()).unwrap(),
+            "global_usage",
+            "global_usage",
+        );
+    }
+
+    #[test]
+    fn test_global_usage_ignore() {
+        test_lint(
+            GlobalLint::new(GlobalConfig {
+                ignore_pattern: "^_.*_$".to_owned(),
+            })
+            .unwrap(),
+            "global_usage",
+            "global_usage_ignore",
+        );
     }
 }
