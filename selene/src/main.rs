0addbc86b6b6c2a5589005ce6a350be1d02579a5
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-10-14 18:20:07 -0700

    Remove debugging print

--- selene/src/main.rs
+++ selene/src/main.rs
@@ -238,8 +238,6 @@ fn apply_diagnostics_fixes(code: &str, diagnostics: &Vec<&Diagnostic>) -> String
             }
         });
 
-    println!("{}", chosen_diagnostics.iter().count());
-
     new_code
 }
 
@@ -359,14 +357,6 @@ fn read<R: Read>(
                     .map(|diagnostic| &diagnostic.diagnostic)
                     .filter(|diagnostic| diagnostic.fixed_code.is_some())
                     .collect(),
-            );
-
-            println!(
-                "Fixer generated invalid code:\n\
-                ----------------\n\
-                {}\n\
-                ----------------\n",
-                fixed_code
             );
 
             let fixed_ast = full_moon::parse(&fixed_code).expect(
