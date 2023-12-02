d2e3e4986abe1a2833e6848c6d5cccd5a059bc2f
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-12-02 09:46:13 -0800

    Test negative

--- selene-lib/tests/lints/approx_constant/approx_constant.lua
+++ selene-lib/tests/lints/approx_constant/approx_constant.lua
@@ -2,6 +2,7 @@ local good = 3
 local good = 3.1
 local good = 3.13
 local good = 3.15
+local good = -3.15
 local good = 3.1417
 local good = 3.14159266
 
@@ -10,9 +11,11 @@ local good = 3_14
 
 local bad = 3.14
 local bad = 3.141
+local bad = -3.141
 local bad = 3.142
 local bad = 3.1415
 local bad = 3.14159265
 
 local bad = 3.14 + 1
 local bad = f(3.14 + 1)
+local bad = f(-3.14 + 1)
