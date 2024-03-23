cf83333c389b00d30e789efb461b0034bc030c1b
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2024-03-23 13:17:41 -0700

    Remove space

--- selene-vscode/src/extension.ts
+++ selene-vscode/src/extension.ts
@@ -150,7 +150,7 @@ export async function activate(
         const output = await selene.seleneCommand(
             context.globalStorageUri,
             // Must use relative path for now due to https://github.com/Kampfkarren/selene/issues/593
-            "--display-style=json2 --no-summary " +
+            "--display-style=json2 --no-summary" +
                 vscode.workspace.asRelativePath(document.uri.fsPath),
             selene.Expectation.Stderr,
             vscode.workspace.getWorkspaceFolder(document.uri),
