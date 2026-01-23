28ab8f168c4a2fcb5c25da3032b07c6155147e31
Kampfkarren <boynedmaster@gmail.com>
2026-01-22 20:07:28 -0800

    Clippy lints

--- selene-lib/src/standard_library/mod.rs
+++ selene-lib/src/standard_library/mod.rs
@@ -819,18 +819,13 @@ impl Visitor<'_> for RequiredVisitor {
     }
 }
 
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub enum Observes {
+    #[default]
     ReadWrite,
     Read,
     Write,
-}
-
-impl Default for Observes {
-    fn default() -> Self {
-        Self::ReadWrite
-    }
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, Deserialize, Serialize)]
