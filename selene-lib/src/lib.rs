41e2fe38b79bdd0fa6f036f9776324b67c5dd7cc
James <jamestran201@gmail.com>
2026-02-25 16:45:31 +0700

    replace tab with space

--- selene-lib/src/lib.rs
+++ selene-lib/src/lib.rs
@@ -319,7 +319,7 @@ use_lints! {
     undefined_variable: lints::undefined_variable::UndefinedVariableLint,
     unscoped_variables: lints::unscoped_variables::UnscopedVariablesLint,
     unused_variable: lints::unused_variable::UnusedVariableLint,
-	variable_naming: lints::variable_naming::VariableNamingLint,
+    variable_naming: lints::variable_naming::VariableNamingLint,
 
     #[cfg(feature = "roblox")]
     {
@@ -329,3 +329,4 @@ use_lints! {
         roblox_suspicious_udim2_new: lints::roblox_suspicious_udim2_new::SuspiciousUDim2NewLint,
     },
 }
+
